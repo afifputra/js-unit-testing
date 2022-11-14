f100f2620e286b175443bc5c886e4ccdebf929f4
afifputra <afifputraharyono@gmail.com>
2022-11-14 16:02:08 +0700

    add more testing

--- 1-basic-testing/basic-testing-frontend/src/math.test.js
+++ 1-basic-testing/basic-testing-frontend/src/math.test.js
@@ -13,10 +13,36 @@ it("should summarize all number values in an array", () => {
   expect(result).toBe(expectedResult);
 });
 
-it("should return 0 if the array is empty", () => {
-  const numbers = [];
+it("should yield NaN if a least one invalid number is provided", () => {
+  const inputs = ["invalid", 2];
 
-  const result = add(numbers);
-  const expectedResult = 0;
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const inputs = ["1", "2", "3"];
+
+  const result = add(inputs);
+
+  const expectedResult = numbers.reduce((a, b) => a + b, 0);
+
   expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if an empty array is provided", () => {
+  const inputs = [];
+
+  const result = add(inputs);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
 });
