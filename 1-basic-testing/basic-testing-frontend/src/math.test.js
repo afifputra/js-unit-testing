4002abdc6838b365e2df0655293fb2d753fe7a4e
afifputra <afifputraharyono@gmail.com>
2022-11-19 12:44:01 +0700

    core of testing

--- 1-basic-testing/basic-testing-frontend/src/math.test.js
+++ 1-basic-testing/basic-testing-frontend/src/math.test.js
@@ -21,16 +21,6 @@ it("should yield NaN if a least one invalid number is provided", () => {
   expect(result).toBeNaN();
 });
 
-it("should yield a correct sum if an array of numeric string values is provided", () => {
-  const inputs = ["1", "2", "3"];
-
-  const result = add(inputs);
-
-  const expectedResult = numbers.reduce((a, b) => a + b, 0);
-
-  expect(result).toBe(expectedResult);
-});
-
 it("should yield 0 if an empty array is provided", () => {
   const inputs = [];
 
@@ -42,6 +32,17 @@ it("should yield 0 if an empty array is provided", () => {
 it("should throw an error if no value is passed into the function", () => {
   const resultFn = () => {
     add();
+  };
+
+  expect(resultFn).toThrow();
+});
+
+it("should throw an error if provided with multiple arguments instead of an array", () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
   };
 
   expect(resultFn).toThrow();
