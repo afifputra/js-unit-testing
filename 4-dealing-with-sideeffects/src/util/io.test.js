34b6b4643e0f85f811b0486c24dce0cfdb4378ea
afifputra <afifputraharyono@gmail.com>
2023-03-11 09:31:55 +0700

    using mocks

--- 4-dealing-with-sideeffects/src/util/io.test.js
+++ 4-dealing-with-sideeffects/src/util/io.test.js
@@ -1,14 +1,17 @@
-import { it, expect, describe } from "vitest";
+import { it, expect, describe, vi } from "vitest";
+import { promises as fs } from "fs";
 
 import writeData from "./io.js";
 
+vi.mock("fs");
+
 describe("writeData", () => {
-  it("should execute function writeData", async () => {
+  it("should execute function writeFile", async () => {
     const data = "Hello World";
     const fileName = "test.txt";
 
-    const file = await writeData(data, fileName);
+    await writeData(data, fileName);
 
-    expect(file).toBeUndefined();
+    expect(fs.writeFile).toBeCalled();
   });
 });
