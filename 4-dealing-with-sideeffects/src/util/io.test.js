a703fba465c6a4b17197d52bdb1f58dd570d6345
afifputra <afifputraharyono@gmail.com>
2023-04-20 09:16:05 +0700

    using mocks for make implementation of side effects

--- 4-dealing-with-sideeffects/src/util/io.test.js
+++ 4-dealing-with-sideeffects/src/util/io.test.js
@@ -4,6 +4,15 @@ import { promises as fs } from "fs";
 import writeData from "./io.js";
 
 vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 describe("writeData", () => {
   it("should execute function writeFile", async () => {
@@ -12,6 +21,7 @@ describe("writeData", () => {
 
     await writeData(data, fileName);
 
-    expect(fs.writeFile).toBeCalled();
+    // expect(fs.writeFile).toBeCalled();
+    expect(fs.writeFile).toBeCalledWith(fileName, data);
   });
 });
