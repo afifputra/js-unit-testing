ff5784c8481deaf60149d890d2b3c2699a62871e
afifputra <afifputraharyono@gmail.com>
2023-04-20 14:11:34 +0700

    make http test

---
+++ 5-deeper-mocks-fe/util/http.test.js
@@ -0,0 +1,29 @@
+import { it, expect, vi } from "vitest";
+
+import { sendDataRequest } from "./http";
+
+const testResponseData = { testKey: "testData" };
+
+const testFetch = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    const testResponse = {
+      ok: true,
+      json: () => {
+        return new Promise((resolve, reject) => {
+          resolve(testResponseData);
+        });
+      },
+    };
+    resolve(testResponse);
+  });
+});
+
+vi.stubGlobal("fetch", testFetch);
+
+it("should return any available response data", async () => {
+  const testData = { key: "test" };
+
+  const responseData = await sendDataRequest(testData);
+
+  expect(responseData).toEqual(testResponseData);
+});
