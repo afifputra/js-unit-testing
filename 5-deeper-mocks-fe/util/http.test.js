c938cd774527a6eeccad9034ca54a853eb5afd92
afifputra <afifputraharyono@gmail.com>
2023-04-27 10:52:17 +0700

    using mock implementation once if the same function is called multiple times

--- 5-deeper-mocks-fe/util/http.test.js
+++ 5-deeper-mocks-fe/util/http.test.js
@@ -1,6 +1,7 @@
 import { it, expect, vi } from "vitest";
 
 import { sendDataRequest } from "./http";
+import { HttpError } from "./errors";
 
 const testResponseData = { testKey: "testData" };
 
@@ -42,4 +43,32 @@ it("should convert the provided data to JSON before sending the request", async 
   }
 
   expect(errMessage).toBeNull();
+});
+
+it("should throw an HttpError in case of non-ok responses", async () => {
+  testFetch.mockImplementationOnce((url, options) => {
+    return new Promise((resolve, reject) => {
+      const testResponse = {
+        ok: false,
+        json: () => {
+          return new Promise((resolve, reject) => {
+            resolve(testResponseData);
+          });
+        },
+      };
+      resolve(testResponse);
+    });
+  });
+
+  const testData = { key: "test" };
+
+  let errMessage = null;
+
+  try {
+    await sendDataRequest(testData);
+  } catch (error) {
+    errMessage = error;
+  }
+
+  expect(errMessage).toBeInstanceOf(HttpError);
 });
