dac96e29c7a1abb57229b65bf5f1594f641bd7cc
afifputra <afifputraharyono@gmail.com>
2023-04-28 10:39:57 +0700

    update

--- 5-deeper-mocks-fe/posts/posts.test.js
+++ 5-deeper-mocks-fe/posts/posts.test.js
@@ -6,16 +6,16 @@ const testTitle = "Test Title";
 const testContent = "Test Content";
 let testFormData = null;
 
-beforeEach(() => {
-  // make a fake form object
-  testFormData = {
-    get: (key) => {
-      return key === "title" ? testTitle : testContent;
-    },
-  };
-});
-
 describe("extractPostData", () => {
+  beforeEach(() => {
+    // make a fake form object
+    testFormData = {
+      get: (key) => {
+        return key === "title" ? testTitle : testContent;
+      },
+    };
+  });
+
   it("should extract the title and content from a form object", () => {
     const data = extractPostData(testFormData);
 
